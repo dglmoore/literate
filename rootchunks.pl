3fa49661caa8c9bddfa3332989f27a4a853646ef
Douglas Moore <doug@dglmoore.com>
2015-09-05 12:08:05 -0500

    redefine "root chuncks" as "chunks that are never referenced"

--- rootchunks.pl
+++ rootchunks.pl
@@ -5,16 +5,23 @@
 # license that can be found in the LICENSE file.
 
 my %roots;
+my %leaves;
 
 my $linenum = 0;
 while (<>) {
   $linenum += 1;
-  if (/^\s*\\chunk\{([^\} ]+)\}\s*$/) {
+  if (/^\s*\\chunk\{([^\}]+)\}\s*$/) {
     $roots{$1} = 1;
   } elsif (/^\s*\\chunk\{\s*\}\s*$/) {
     print STDERR "WARNING: unnamed chunk found on line $linenum\n";
+  } elsif (/^\s*\\getchunk\{([^\}]+)\}\s*$/) {
+    $leaves{$1} = 1;
+  } elsif (/^\s*\\getchunk\{\s*\}\s*$/) {
+    print STDERR "WARNING: unnamed chunk requested on line $linenum\n";
   }
 }
+
 for (keys %roots) {
-	print "$_\n";
+	print "$_\n" if not exists $leaves{$_};
 }
+
